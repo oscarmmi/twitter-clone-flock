f5d496cdd6ba364c141a137a1376a9e96732244f
Oscar Munoz <omunoz@visbl.com>
2026-04-04 09:40:39 -0500

    changing favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
 
         <!-- Scripts -->
         @vite(['resources/js/app.js', 'resources/css/app.css'])
